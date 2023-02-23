7aeead278010a2ac9bc107041434f5f8b3c3857d
BeenMoon <111475800+BeenMoon@users.noreply.github.com>
2023-02-23 18:07:17 +0900

    Update nps_summary.py

--- config/nps_summary.py
+++ config/nps_summary.py
@@ -1,8 +1,8 @@
-{
- 'schema': 'wkdansqls',
- 'table': 'nps_summary',
- 'primary_key': 'date',
- 'main_sql': """
+dictionary = {
+  'schema': 'wkdansqls',
+  'table': 'nps_summary',
+  'primary_key': 'date',
+  'main_sql': """
      SELECT DATE(created_at) AS date
           , SUM(CASE WHEN score >= 9 THEN 1
                      WHEN score <= 6 THEN -1 END)::FLOAT
@@ -10,13 +10,13 @@
      FROM wkdansqls.nps
      GROUP BY date;
      """,
- 'input_test': [
+  'input_test': [
          {
              'sql': "SELECT COUNT(1) FROM wkdansqls.nps;",
              'count': 150000
          }
      ],
- 'output_test': [
+  'output_test': [
          {
              'sql': "SELECT COUNT(1) FROM wkdansqls.temp_nps_summary;",
              'count': 12
