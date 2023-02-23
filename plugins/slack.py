4daba23f15dbd57c990a00a946097172f4b62606
BeenMoon <111475800+BeenMoon@users.noreply.github.com>
2023-02-23 19:38:35 +0900

    Update slack.py

--- plugins/slack.py
+++ plugins/slack.py
@@ -3,8 +3,8 @@ from airflow.models import Variable
 
 def send_slack_success(context) -> None:
     client = WebClient(Variable.get('slack_api_token'))
-    client.chat_postMessage(channel='#일반', text=f"The DAG {context['dag_id']} succeeded", username="Airflow")
+    client.chat_postMessage(channel='#일반', text=f"The DAG {context['run_id']} succeeded", username="Airflow")
     
 def send_slack_failure(context) -> None:
     client = WebClient(Variable.get('slack_api_token'))
-    client.chat_postMessage(channel='#일반', text=f"The DAG {context['dag_id']} failed", username="Airflow")
+    client.chat_postMessage(channel='#일반', text=f"The DAG {context['run_id']} failed", username="Airflow")
